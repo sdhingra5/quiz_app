7cd7b01aeb7476b335243eb5e635b1a8f78d982e
sdhingra5 <sidaqdhingra4@gmail.com>
2026-02-12 11:32:53 -0500

    Created Question data model and list of questions data

---
+++ lib/data/questions.dart
@@ -0,0 +1,15 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const question = [
+  QuizQuestion(
+    'How did Edward Elric lose his arm in FMA?'
+    ,
+   [
+    'Putting his brothers sould in a suit of armor',
+    'In a Duel',
+    'In a break Dabbing Accident',
+    'He never had any arms',
+    ],
+   ),
+
+];
