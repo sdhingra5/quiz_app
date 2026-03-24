0cef71ab378d0ff696bc9ceb69da600aa3d34479
sdhingra5 <sidaqdhingra4@gmail.com>
2026-03-23 21:49:05 -0400

    updated and added stuff to the question_summary.dart

--- lib/question_summary.dart
+++ lib/question_summary.dart
@@ -1,34 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionSummary extends StatelessWidget {
   const QuestionSummary({super.key, required this.summaryData});
 
   final List <Map<String,Object>> summaryData;
-  @override
  
+  @override
   //TODO: implement build
   Widget build(context){
     return Column(
       children: summaryData.map(
         (data){
-          return Row(
+          return Container(
+            margin: const EdgeInsets.only(bottom: 20),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
             children:[
-              Text(((data['question_index'] as int) +1).toString()),
+              Container(
+                width: 40,
+                height: 40,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: data['user_answer'] == data['correct_answer'] 
+                  ? const Color.fromARGB(255, 102, 255, 178) 
+                  : const Color.fromARGB(255, 255, 120, 120),
+                  shape: BoxShape.circle,
+              ),
+              child: Text(
+                ((data['question_index'] as int) +1).toString(),
+              style: GoogleFonts.lato(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+              ),
+            ),
+          
+              const SizedBox(width: 20,),
+
               Expanded(
                 child: Column(
-                  children:[
-                    Text(data['question'] as String),
-                    const SizedBox(height:5,),
-                    Text(data['user_answer'] as String),
-                    Text(data['correct_answer'] as String),
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      data['question'] as String,
+                      style: GoogleFonts.lato(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),  
+                 
+                    const SizedBox(height: 8),
+
+
+                    Text(
+                      'Your answer: ${data['user_answer']}',
+                      style: GoogleFonts.lato(
+                        color: const Color.fromARGB(255, 255, 120, 120),
+                        fontSize: 16,
+                      ),    
+                    ),
+                  const SizedBox(height: 4),
+                  
+                    Text(
+                      'Correct answer: ${data['correct_answer']}',
+                      style: GoogleFonts.lato(
+                        color: const Color.fromARGB(255, 102, 255, 178),
+                        fontSize: 16,
+                      ),
+                    ),
                   ],
                 ),
               )
-            ],
-          );
-         }
-        ).toList(),
-      );
-    }
-  }
+            
+          
+          ],
+          ),      
 
+        );
+      },
+      ).toList(),
+
+    );
+  }
+  
+  
+
+}
